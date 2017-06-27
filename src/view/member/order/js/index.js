9d61e561001db86002ac1f3270630bf335feada4
blusoul <blusoul@users.noreply.github.com>
2017-06-27 23:03:40 +0800

    delete waste files

--- src/view/member/order/js/index.js
+++ src/view/member/order/js/index.js
@@ -1,1 +1,22 @@
 console.log('这是订单页');
+
+function* gen() {
+  yield 1;
+  yield 2;
+}
+
+var mm = gen();
+
+console.log(mm.next());
+console.log(mm.next())
+
+function foo() {
+  console.log(a)
+}
+
+function bar() {
+  var a = 3;
+  foo();
+}
+var a = 1;
+bar();
